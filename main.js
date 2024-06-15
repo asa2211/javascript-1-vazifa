06a4b062c96ccb28935fd9bb53f58df5104de320
sarvar <sarvar@gmail.com>
2024-06-15 13:03:34 +0500

    1-vazifa

---
+++ main.js
@@ -0,0 +1,13 @@
+var name = prompt('ismingiz?');
+var age = prompt('yoshingiz?');
+var misol1 = prompt('misolni ishlang: 9+4=')
+var misol2 = prompt('misolni ishlang: 9*1=')
+var misol3 = prompt('misolni ishlang: 8/4=')
+var misol4 = prompt('misolni ishlang: 11%3=')
+alert("consolga chiqarishni tasdiqlaysizmi")
+console.log('ism:', name)
+console.log('yosh:', age)
+console.log('9 + 4 = 13 sizning javobingiz:',misol1)
+console.log('9 * 1 = 9 sizning javobingiz:',misol2)
+console.log('8 / 4 = 2 sizning javobingiz:',misol3)
+console.log('11 % 3 = 2 sizning javobingiz:',misol4)
